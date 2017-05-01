565e50a1032d0a877f03b1d4386d2ebffe661410
Albert St. Aubin <astaubin@edx.org>
2017-05-01 11:59:52 -0400

    Updated the message and style of the status notification when a user enters text into numerical input

--- common/lib/xmodule/xmodule/tests/test_capa_module.py
+++ common/lib/xmodule/xmodule/tests/test_capa_module.py
@@ -819,6 +819,9 @@ class CapaModuleTest(unittest.TestCase):
 
             # Expect an AJAX alert message in 'success'
             expected_msg = 'Error: test error'
+            if exception_class == StudentInputError:
+                expected_msg = 'test error'
+
             self.assertEqual(expected_msg, result['success'])
 
             # Expect that the number of attempts is NOT incremented
@@ -885,8 +888,11 @@ class CapaModuleTest(unittest.TestCase):
                 get_request_dict = {CapaFactory.input_key(): '3.14'}
                 result = module.submit_problem(get_request_dict)
 
-            # Expect an AJAX alert message in 'success'
             expected_msg = u'Error: ȧƈƈḗƞŧḗḓ ŧḗẋŧ ƒǿř ŧḗşŧīƞɠ'
+            # Expect an AJAX alert message in 'success'
+            if exception_class == StudentInputError:
+                expected_msg = u'ȧƈƈḗƞŧḗḓ ŧḗẋŧ ƒǿř ŧḗşŧīƞɠ'
+
             self.assertEqual(expected_msg, result['success'])
 
             # Expect that the number of attempts is NOT incremented
